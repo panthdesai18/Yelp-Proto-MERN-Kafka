c4d63559c6e7dce0d4ac96c3f06e5a52752fc97a
Panth Desai <55039829+panthdesai18@users.noreply.github.com>
2021-07-27 16:40:09 -0700

    Update getCustCancelledOrders.js

--- kafka-backend/services/getCustCancelledOrders.js
+++ kafka-backend/services/getCustCancelledOrders.js
@@ -4,7 +4,6 @@ var Orders = require('../models/Orders')
 function handle_request(msg,callback){
     console.log(msg)
     console.log("Getting Rest Received Orders!")
-    // var user = "SELECT orderid,dishid,quantity from yelp_proto.orderdetails where orderid in (SELECT orderid FROM yelp_proto.order where restid ='"+req.body.userid+"'); ";
     var final = []
     Orders.find({userid:msg.userid,status:"Cancelled"},function(err, result1){
         if(err) throw err;
